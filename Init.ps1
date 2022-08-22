a2bf67b9357fc9e23f58f7196f14801e5c674696
jared-sstechs <LuwgazCwE0D>
2022-08-22 18:17:13 -0400

    Added Switch to automate depending on the state.

--- Init.ps1
+++ Init.ps1
@@ -1,2 +1,42 @@
+# Utilizing David Segura's functions
 iex (irm sandbox.osdcloud.com)
-Write-Output "This is fun!"
+
+# David stores the state of the computer in $WindowsPhase, i.e.  the computer is 
+# booted to WinPe, at OOBE, or within Windows, etc.
+
+# David has several useful functions built into sandbox.osdcloud.com, which
+# is called from functions.osdcloud.com.  We will utilize some of these.
+# https://www.osdcloud.com/sandbox/functions
+#    AddCapability
+#    NetFX
+#    RemoveAppx
+#    Rsat
+#    UpdateDrivers
+#    UpdateWindows
+
+Switch ($WindowsPhase) {
+    "WinPE" {
+        Start-OSDCloud -OSName 'Windows 10 21H2 x64' -OSEdition Enterprise -OSLanguage en-us -OSLicense Volume -Restart
+    }
+
+    "OOBE" {
+        AddCapability
+        RemoveAppx
+        UpdateDrivers
+        UpdateWindows
+        Write-Host -ForegroundColor "Congratz! All done!"
+    }
+
+    "Windows" {
+        Write-Host -ForegroundColor Cyan "No functions were automatically called."
+        Write-Host -ForegroundColor Gray @'
+#        AddCapability
+#        NetFX
+#        RemoveAppx
+#        Rsat
+#        UpdateDrivers
+#        UpdateWindows
+'@
+
+    }
+}
