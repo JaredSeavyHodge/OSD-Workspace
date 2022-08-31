990f92db6dfcdc6207d353fa311a8945a75168b4
Hodge, Jared <jhodge5@hccfl.edu>
2022-08-31 14:06:47 -0400

    Added a choice menu to AD or AAD join.

--- Start-HCCDeploy.ps1
+++ Start-HCCDeploy.ps1
@@ -1,5 +1,5 @@
 # Call this script with 
-#   iex (irm https://tinyurl.com/osdcloud)
+#   iex (irm https://tinyurl.com/HCCDeploy)
 
 # Utilizing David Segura's functions
 iex (irm sandbox.osdcloud.com)
@@ -96,18 +96,100 @@ function InstallSoftware {
     }
 }
 
+function ChoiceMenu {
+    param(
+        [String]$Title,
+        [String]$Message,
+        [String[]]$Options,
+        [Int]$Default=0
+    )
+    $Answers = @()
+    foreach ($Answer in $Options) {
+        $Answers += New-Object System.Management.Automation.Host.ChoiceDescription "&$Answer", $Answer
+
+    }
+
+
+    # $Academic = New-Object System.Management.Automation.Host.ChoiceDescription '&Academic', 'Academic.hccfl.edu'
+    # $Family = New-Object System.Management.Automation.Host.ChoiceDescription '&Family', 'Family.hccfl.edu'
+    # $None = New-Object System.Management.Automation.Host.ChoiceDescription '&None', 'Do not join.'
+    switch ($Answers.count) {
+        0 { Write-Error "You must enter an answer option." }
+        1 { Write-Error "You must enter more than 1 answer option."}
+        2 { $OptionObj = [System.Management.Automation.Host.ChoiceDescription[]]($Answers[0], $Answers[1]) }
+        3 { $OptionObj = [System.Management.Automation.Host.ChoiceDescription[]]($Answers[0], $Answers[1], $Answers[2]) }
+        4 { $OptionObj = [System.Management.Automation.Host.ChoiceDescription[]]($Answers[0], $Answers[1], $Answers[2], $Answers[3]) }
+        5 { Write-Error "Only 4 options are allowed."}
+        Default {5}
+    }
+    # $OptionObj = [System.Management.Automation.Host.ChoiceDescription[]]($Academic, $Family, $None)
+
+    # $title = 'Domain'
+    # $message = 'Which domain would you join?'
+    $result = $host.ui.PromptForChoice($title, $message, $OptionObj, $Default)
+    $result
+}
+
+function TryDomainJoin {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $Domain,
+        $ComputerName
+    )
+    $Credential = Get-Credential -Message "Enter your domain credentials to join. ( $domain\<username> )"
+    try {
+        Rename-Computer $ComputerName
+        Write-Host -ForegroundColor White -BackgroundColor DarkMagenta "Joining $Domain"
+        Add-Computer -Domain $Domain -NewName $ComputerName -Credential $Credential -Restart -Force
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+    
+}
+
 Switch ($WindowsPhase) {
     "WinPE" {
         Start-OSDCloud -OSName 'Windows 10 21H2 x64' -OSEdition Enterprise -OSLanguage en-us -OSLicense Volume -Restart
     }
 
     "OOBE" {
+
         AddCapability -Name "Print.Management*"
         RemoveAppx people,xbox,phone,GamingApp
         NetFX
         UpdateDrivers
         UpdateWindows
-        Write-Output "If this machine is to be Domain joined. Use the command `'InstallSoftware`'."
+
+        $ComputerName = Read-Host "Computer Name:"
+        $Options = "Azure AD", "Family.hccfl.edu", "Academic.hccfl.edu", "None"
+        $Result = ChoiceMenu -Title "Domain to Join:" -Message "Azure AD should be chosen most of the time." -Options $Options -Default 3
+
+        switch ($Result) {
+            0 {
+                Install-Module AADInternals
+                Get-AADIntAccessTokenForAADJoin -BPRT $BPRT -SaveToCache
+                Join-AADIntDeviceToAzureAD -DeviceName $ComputerName
+            }
+            1 {
+                Write-Output "Preparing the computer for on-prem AD join."
+                Write-Output "Installing Software"
+                InstallSoftware
+                TryDomainJoin -ComputerName $ComputerName -Domain "family.hccfl.edu"
+            }
+            2 {
+                Write-Output "Preparing the computer for on-prem AD join."
+                Write-Output "Installing Software"
+                InstallSoftware
+                TryDomainJoin -ComputerName $ComputerName -Domain "academic.hccfl.edu"
+            }
+            3 {
+                Write-Warning "Not joining a domain.  Renaming the computer and rebooting."
+                Rename-Computer $ComputerName -Force -Restart
+            }
+        }
     }
 
     "Windows" { 
